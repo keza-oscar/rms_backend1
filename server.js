39951efe08e374512fe35216f62f8f18c3853ba3
keza-oscar <williamoscar0304@gmail.com>
2026-07-28 16:38:27 +0300

    Optimize DB pool settings for faster connections

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ app.get('/', (req, res) => {
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000,
 });
 
 pool.on('connect', () => {
